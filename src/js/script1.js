b0b292a198ccd6969f865a278df92a4aa583fa59
unknown <legends15231@gmail.com>
2025-02-05 10:07:58 -0600

    agregue un script No2

--- src/js/script1.js
+++ src/js/script1.js
@@ -1,1 +1,88 @@
-console.log("hola mundo")
+// comentario de una sola linea
+/*
+    este es un comentario de bloque
+*/
+
+/** 
+ * este es un comentario de docuemtacion
+ */
+
+//asignacion de variables
+let numero = 56;
+
+//expresion aritmetica
+let multiplicacion = 5 * 32;
+
+//expresion algebraica
+let x;
+let expresion = 5(x)-3(x)+1;
+
+//llamar una funcion
+sumar();
+calcularpromedio();
+
+//estructura de una funcion sin valor de retorno
+function sumar(x,y){
+
+}
+
+//estructura de una funcion sin valor de retorno
+function getPromedio() {
+    return promedio, x, y;
+}
+
+function name(params) {
+    return "Buenas";
+}
+//estructuras de control (if)
+//simple
+if(condicion){
+
+}
+
+//doble
+if(condicion){
+
+}else{
+
+}
+
+//multiple 1
+if (condition) {
+    if (condition) {
+    
+    } else {
+    
+    }
+}
+
+// multiple 2
+if (condition) {
+    
+}
+else if (condition) {
+    
+}
+else if (condition) {
+    
+}
+
+//blucles
+for (let i= 0; i <=10; i++) {
+
+}
+
+//estructura de una clase
+class miclase {
+    nombre="angel";
+    edad="22";
+    constructor(apellidoP,apellidoM) {
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.gender = "F";
+    }
+}
+
+//manipulacion de objetos y propiedades
+persona.numControl="19920174";
+
---
+++ src/js/script2.js
@@ -0,0 +1,5 @@
+console.log("hola son un script")
+alert("alerta desde un script independiente")
+
+let nombre = prompt("ingresa un nombre")
+console.log; (nombre)
